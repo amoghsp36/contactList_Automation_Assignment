147915e16c5a428cea054934d31881cee0d44f01
amoghp <amoghp@testvagrant.com>
2023-10-18 18:38:27 +0530

    final commit

--- app/src/test/java/contactlist_apiautomation_assignment/E2E_Contact.java
+++ app/src/test/java/contactlist_apiautomation_assignment/E2E_Contact.java
@@ -26,94 +26,112 @@ public class E2E_Contact {
     public static String _id;
 
     @Test(dataProvider = "login_user_data", dataProviderClass = DataProvider.class,priority = 1)
-    public void user_login(String email, String password) throws IOException {
+    public void should_user_login(String email, String password) throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()).body(buildData.userLoginData(email, password));
         ApiResources apiResource = ApiResources.valueOf("UserLoginAPI");
         System.out.println(apiResource);
+        //Act
         response = req.when().post(apiResource.getResource()).then().extract().response();
         LoginResp loginResp = response.as(LoginResp.class);
         loginToken = loginResp.getToken();
         System.out.println(loginToken);
         long respTime = 2500;
         long actualRespTime = response.getTime();
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
         Assert.assertTrue(actualRespTime <= respTime);
         //Assert.assertEquals(loginResp.getUser().getEmail(),"contact_list25@gmail.com");
     }
 
     @Test(dataProvider = "add_contact_data", dataProviderClass = DataProvider.class, priority = 2)
-    public void add_contact(String fname, String lname, String birthdate, String email, String phone, String street1, String street2, String city, String state, String postalCode, String country) throws IOException {
-
+    public void should_add_contact(String fname, String lname, String birthdate, String email, String phone, String street1, String street2, String city, String state, String postalCode, String country) throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()
                 .header("Authorization","Bearer "+loginToken))
                 .body(buildData.addContactData(fname,lname,birthdate,email,phone,street1,street2,city,state,postalCode,country));
         apiResources = ApiResources.valueOf("AddContactAPI");
         System.out.println(loginToken);
         System.out.println(apiResources.getResource());
+        //Act
         response = req.when().post(apiResources.getResource()).then().extract().response();
         AddContactResp addContactResp = response.as(AddContactResp.class);
         _id = addContactResp.getId();
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),201);
     }
 
     @Test(priority = 3)
-    public void get_contact() throws IOException {
+    public void should_get_contact() throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()
                 .header("Authorization","Bearer "+loginToken));
         apiResources = ApiResources.valueOf("GetContactAPI");
         System.out.println(apiResources.getResource());
-        response = req.when().get(apiResources.getResource()).then().extract().response();
+        //Act
+        response = req.when().get(apiResources.getResource()+_id).then().extract().response();
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
 
     @Test
-    public void get_contact_list() throws IOException {
+    public void should_get_contact_list() throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()
                 .header("Authorization","Bearer "+loginToken));
         apiResources = ApiResources.valueOf("GetContactListAPI");
         System.out.println(apiResources.getResource());
+        //Act
         response = req.when().get(apiResources.getResource()).then().extract().response();
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
 
     @Test(dataProvider = "update_contact_data", dataProviderClass = DataProvider.class, priority = 4)
-    public void update_contact(String fname, String lname, String birthdate, String email, String phone, String street1, String street2, String city, String state, String postalCode, String country) throws IOException {
-
+    public void should_update_contact(String fname, String lname, String birthdate, String email, String phone, String street1, String street2, String city, String state, String postalCode, String country) throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()
                 .header("Authorization", "Bearer "+loginToken))
                 .body(buildData.addContactData(fname,lname,birthdate,email,phone,street1,street2,city,state,postalCode,country));
         apiResources = ApiResources.valueOf("UpdateContactAPI");
 
         System.out.println(apiResources.getResource());
+        //Act
         response = req.when().put(apiResources.getResource()+_id).then().extract().response();
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
 
     @Test(dataProvider = "patch_contact_data", dataProviderClass = DataProvider.class, priority = 5)
-    public void update_patch_contact(String fname, String lname, String birthdate) throws IOException {
+    public void should_update_patch_contact(String fname, String lname, String birthdate) throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()
                 .header("Authorization", "Bearer "+loginToken))
                 .body(buildData.patchContactData(fname, lname, birthdate));
         apiResources = ApiResources.valueOf("UpdateContactPatchAPI");
         System.out.println(apiResources.getResource());
+        //Act
         response = req.when().patch(apiResources.getResource()+_id).then().extract().response();
-
+        //Assert
         Assert.assertEquals(response.statusCode(),200);
     }
 
     @Test(priority = 6)
-    public void delete_contact() throws IOException {
+    public void should_delete_contact() throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()
                 .header("Authorization", "Bearer "+loginToken));
         apiResources = ApiResources.valueOf("DeleteContactAPI");
         System.out.println(apiResources.getResource());
+        //Act
         response = req.delete(apiResources.getResource()+_id).then().extract().response();
         System.out.println(response.asString());
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
         Assert.assertEquals(response.asString(),"Contact deleted");
     }
--- app/src/test/java/contactlist_apiautomation_assignment/E2E_user.java
+++ app/src/test/java/contactlist_apiautomation_assignment/E2E_user.java
@@ -27,13 +27,15 @@ public class E2E_user {
     public static String loginToken;
 
     @Test(dataProvider = "user_data", dataProviderClass = DataProvider.class,priority = 1)
-    public void add_user(String fname, String lname, String email, String password,String method) throws IOException {
+    public void should_add_user(String fname, String lname, String email, String password,String method) throws IOException {
+        //Arrange
         req = given()
                 .spec(globalProperties.getReq())
                 .body(buildData.addUserData(fname, lname, email, password));
         ApiResources apiResource = ApiResources.valueOf("AddUserAPI");
         System.out.println(apiResource.getResource());
 
+        //Act
         if(method.equalsIgnoreCase("POST")){
             response = req.when().post(apiResource.getResource()).then().extract().response();
             AddUserResp bearerToken = response.as(AddUserResp.class);
@@ -48,63 +50,83 @@ public class E2E_user {
             response = req.when().patch(apiResource.getResource()).then().extract().response();
             System.out.println(response.prettyPrint());
         }
+
+        //Assert
         Assert.assertEquals(response.getStatusCode(),201);
     }
 
     @Test(dataProvider = "login_user_data", dataProviderClass = DataProvider.class,priority = 2)
-    public void user_login(String email, String password) throws IOException {
+    public void should_user_login(String email, String password) throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq()).body(buildData.userLoginData(email, password));
         ApiResources apiResource = ApiResources.valueOf("UserLoginAPI");
         System.out.println(apiResource);
+
+        //Act
         response = req.when().post(apiResource.getResource()).then().extract().response();
         LoginResp resp = response.as(LoginResp.class);
         loginToken = resp.getToken();
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
     @Test(priority = 3)
-    public void get_user() throws IOException {
+    public void should_get_user() throws IOException {
+        //Arrange
         System.out.println(token);
         req = given().spec(globalProperties.getReq().header("Authorization","Bearer "+token));
         ApiResources apiResource = ApiResources.valueOf("GetUserAPI");
         System.out.println(apiResource);
+
+        //Act
         response = req.when().get(apiResource.getResource()).then().extract().response();
 
+        //assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
 
     @Test(dataProvider = "update_user_data",dataProviderClass = DataProvider.class,priority = 4)
-    public void update_user(String fname, String lname, String email, String password,String method) throws IOException {
+    public void should_update_user(String fname, String lname, String email, String password,String method) throws IOException {
+        //Arrange
         System.out.println(token);
         req = given().spec(globalProperties.getReq().header("Authorization","Bearer "+token))
                 .body(buildData.addUserData(fname, lname, email, password));
         ApiResources apiResource = ApiResources.valueOf("UpdateUserAPI");
         System.out.println(apiResource);
+
+        //Act
         if(method.equalsIgnoreCase("PATCH")){
             response = req.when().patch(apiResource.getResource()).then().extract().response();
         }
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
     @Test(priority = 6, enabled = false)
-    public void logout_user() throws IOException {
+    public void should_logout_user() throws IOException {
+        //Arrange
         req = given().spec(globalProperties.getReq().header("Authorization","Bearer "+token));
         ApiResources apiResources = ApiResources.valueOf("UserLogout");
         System.out.println(apiResources.getResource());
+        //Act
         response = req.when().post(apiResources.getResource()).then().extract().response();
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
 
     @Test(dataProvider = "login_user_data", dataProviderClass = DataProvider.class, priority = 5)
-    public void delete_user(String email, String password) throws IOException {
-        user_login(email,password);
+    public void should_delete_user(String email, String password) throws IOException {
+        //Arrange
+        should_user_login(email,password);
         req = given().spec(globalProperties.getReq()
                 .header("Authorization", "Bearer "+loginToken));
         System.out.println(loginToken);
         apiResources = ApiResources.valueOf("DeleteUserAPI");
         System.out.println(apiResources.getResource());
+        //Act
         response = req.when().delete(apiResources.getResource()).then().extract().response();
 
+        //Assert
         Assert.assertEquals(response.getStatusCode(),200);
     }
 }
